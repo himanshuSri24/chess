354d5545cc3e54ceebe115e007546b80dd40b8d7
Himanshu Srivastava <84365673+himanshuSri24@users.noreply.github.com>
2024-02-25 11:28:35 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,6 @@ import { Link } from 'react-router-dom'
 
 export default function Footer() {
   return (
-    <div className='footer'>Made with &#x2665; by <Link to="https://www.linkedin.com/in/himanshusri24">&nbsp; Himanshu Srivastava</Link></div>
+    <div className='footer'>Made with &#x2665; by <Link to="https://www.devwithcoffee.com">&nbsp; Himanshu Srivastava</Link></div>
   )
 }
